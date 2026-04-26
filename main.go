b8fd4b9e330bc95b44c4f436b7f0197b6ca68dd3
Ibbolufc <112832891+Ibbolufc@users.noreply.github.com>
2026-04-26 08:29:33 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	configPath := flag.String("config", "/etc/egh-node/config.yml", "Path to config file")
+	nodeID := flag.String("node-id", "", "Node ID for heartbeat registration")
+	flag.Parse()
+
+	cfg, err := LoadConfig(*configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/system", systemHandler)
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	addr := fmt.Sprintf("%s:%d", cfg.API.Host, cfg.API.Port)
+
+	log.Printf("starting egh-node on %s", addr)
+	log.Printf("remote panel: %s", cfg.Remote)
+
+	if *nodeID != "" {
+		go heartbeatLoop(cfg, *nodeID)
+	} else {
+		log.Println("node-id not supplied, heartbeat loop not started")
+	}
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Printf("server stopped: %v", err)
+		os.Exit(1)
+	}
+}
