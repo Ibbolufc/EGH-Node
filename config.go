98a3e8d356299373b9d5e3baa74e10f6d9d7c1e4
Ibbolufc <112832891+Ibbolufc@users.noreply.github.com>
2026-04-26 08:28:09 +0100

    Create config.go

---
+++ config.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Debug  bool   `yaml:"debug"`
+	Remote string `yaml:"remote"`
+	Token  string `yaml:"token"`
+	API    struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+		SSL  struct {
+			Enabled bool `yaml:"enabled"`
+		} `yaml:"ssl"`
+		UploadLimit int `yaml:"upload_limit"`
+	} `yaml:"api"`
+	System struct {
+		Data string `yaml:"data"`
+		SFTP struct {
+			BindPort int `yaml:"bind_port"`
+		} `yaml:"sftp"`
+	} `yaml:"system"`
+	AllowedOrigins []string `yaml:"allowed_origins"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read config: %w", err)
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, fmt.Errorf("parse config: %w", err)
+	}
+
+	if cfg.Remote == "" {
+		return nil, fmt.Errorf("config missing remote")
+	}
+	if cfg.Token == "" {
+		return nil, fmt.Errorf("config missing token")
+	}
+	if cfg.API.Host == "" {
+		cfg.API.Host = "0.0.0.0"
+	}
+	if cfg.API.Port == 0 {
+		cfg.API.Port = 8080
+	}
+
+	return &cfg, nil
+}
